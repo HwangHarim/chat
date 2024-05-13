ac6715dda4cc223db5ad9564e9864b3a0dce9dc3
HwangHarim <scopan01@naver.com>
2024-05-13 13:07:04 +0900

    fix : @PathVariable 수정

--- src/main/java/com/service/chat/message/prasentaion/RedisController.java
+++ src/main/java/com/service/chat/message/prasentaion/RedisController.java
@@ -9,13 +9,7 @@ import com.service.chat.response.ResponseDto;
 import com.service.chat.response.ResponseMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/chat")
@@ -27,7 +21,7 @@ public class RedisController {
     private final ChatRoomService chatRoomService;
 
     @GetMapping("/messages/{roomId}")
-    public ResponseEntity<?> getMessages(@RequestParam Long roomId){
+    public ResponseEntity<?> getMessages(@PathVariable Long roomId) {
         var messages = chatRoomService.getAllRoomChat(roomId);
 
         return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS, messages);
@@ -35,9 +29,9 @@ public class RedisController {
 
     @PostMapping("/messages/{roomId}/{memberId}")
     public ResponseEntity<?> sendMessage(
-        @RequestParam Long roomId,
-        @RequestParam String memberId,
-        @RequestBody ChatMessageRequest chatMessage) {
+            @PathVariable Long roomId,
+            @PathVariable String memberId,
+            @RequestBody ChatMessageRequest chatMessage) {
 
         chatMessage.setRoomId(roomId);
         chatMessage.setSender(memberId);
@@ -48,7 +42,7 @@ public class RedisController {
     }
 
     @GetMapping("/roomIndex/{roomId}")
-    public ResponseEntity<?> getMemberRoom(@RequestParam("roomId") Long roomId){
+    public ResponseEntity<?> getMemberRoom(@PathVariable("roomId") Long roomId) {
         var members = chatRoomService.getAllRoomMember(roomId);
 
         return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS, members);
@@ -56,9 +50,9 @@ public class RedisController {
 
     @PostMapping("/roomIndex/{roomId}/{memberId}")
     public ResponseEntity<?> addMemberRoom(
-        @RequestParam("roomId") Long roomId,
-        @RequestParam("memberId") String memberId
-    ){
+            @PathVariable("roomId") Long roomId,
+            @PathVariable("memberId") String memberId
+    ) {
         var roomIndex = chatRoomService.setMember(new AddAndDeleteMemberRequest(roomId, memberId));
 
         return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS, roomIndex);
@@ -66,9 +60,9 @@ public class RedisController {
 
     @DeleteMapping("/roomIndex/{roomId}/{memberId}")
     public ResponseEntity<?> deleteMemberRoom(
-        @RequestParam("roomId") Long roomId,
-        @RequestParam("memberId") String memberId
-    ){
+            @PathVariable("roomId") Long roomId,
+            @PathVariable("memberId") String memberId
+    ) {
         chatRoomService.deleteMember(new AddAndDeleteMemberRequest(roomId, memberId));
         var members = chatRoomService.getAllRoomMember(roomId);
 
@@ -76,21 +70,21 @@ public class RedisController {
     }
 
     @GetMapping("/rooms")
-    public ResponseEntity<?> getAllRoom(){
+    public ResponseEntity<?> getAllRoom() {
         var allRooms = chatRoomService.getAllRoom();
 
         return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS, allRooms);
     }
 
     @PostMapping("/rooms")
-    public ResponseEntity<?> createRoom(){
+    public ResponseEntity<?> createRoom() {
         var roomId = chatRoomService.createRoom();
 
         return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS, roomId);
     }
 
     @DeleteMapping("/rooms/{roomId}")
-    public ResponseEntity<?> deleteRoom(@RequestParam("roomId") Long roomId){
+    public ResponseEntity<?> deleteRoom(@PathVariable("roomId") Long roomId) {
         chatRoomService.deleteRoom(roomId);
 
         return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS, "삭제 완료");
